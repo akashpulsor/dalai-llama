09141fb2f1ed2f7b0d95e27457f17f9021d7b957
akash tripathi <akashtripathi.2801@gmail.com>
2023-11-15 21:45:07 -0800

    new file:   AppStack.jsx

---
+++ AppStack.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './component/home';
+import Tools from './component/tools';
+import llamaSeo from './component/llamaSeo';
+
+const Stack = createStackNavigator();
+
+export default function AppStack() {
+    console.log("Called Auth Stack");
+  return (
+    
+    <Stack.Navigator  options={{headerShown:false}} initialRouteName='Home'>
+      <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
+      
+      <Stack.Screen name="LLAMASEO" component={llamaSeo} options={{headerShown:false}}/>
+      <Stack.Screen name="Tools" component={Tools} options={{headerShown:false}} />
+    </Stack.Navigator>
+  );
+};
