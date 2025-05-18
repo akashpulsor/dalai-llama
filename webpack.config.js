9032beeccdcec4cc6db7d1aae9dc534a9408933d
akashpulsor <akashtripathi.2801@gmail.com>
2025-05-18 15:48:41 +0530

    Added code for better landing page

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = async function(env = {}, argv = {}) {
+  // Ensure mode is set
+  const mode = argv.mode || 'production';
+  env.mode = mode;
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  config.plugins = config.plugins || [];
+  config.plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-report.html',
+      openAnalyzer: true,
+    })
+  );
+  // Enable code splitting for all chunks
+  config.optimization = {
+    ...config.optimization,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 250000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimize: true,
+  };
+  return config;
+};
